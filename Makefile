9ef02505e11c571e0dc9c4d0f291c51a5098f4c0
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-02-06 11:05:43 -0600

    Pulled over CogSci text and put it in PNAS template

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ creating-words.pdf : creating-words.tex
 	latexmk -pdf -pdflatex=/Library/TeX/texbin/pdflatex creating-words.tex
 	latexmk -c
 creating-words.tex : creating-words.utf.md
-	pandoc creating-words.utf8.md pnas-sections.yaml --to latex --output creating-words.tex --template style/pnas.tex --natbib
+	pandoc creating-words.utf8.md pnas-sections.yaml --to latex --output creating-words.tex --template templates/pnas.tex --natbib
 creating-words.docx : creating-words.Rmd
 		Rscript -e "rmarkdown::render('creating-words.Rmd', output_format = 'word_document')"
 creating-words.utf.md : creating-words.Rmd
--- creating-words.R
+++ creating-words.R
@@ -17,7 +17,21 @@ add_chance <- function(frame) {
     )
 }
 
+count_imitations <- . %>% .$message_id %>% unique() %>% length()
+count_subjects <- . %>% .$subj_id %>% na.omit() %>% unique() %>% length()
+
+
 data("imitations")
+
+n_all_imitations <- count_imitations(imitations)
+n_removed <- imitations %>%
+  filter(rejected == "True") %>%
+  count_imitations()
+n_final_imitations <- imitations %>%
+  filter(rejected == "False") %>%
+  count_imitations()
+n_branches <- imitations$first_gen_id %>% unique() %>% na.omit() %>% length()
+
 
 data("acoustic_similarity_judgments")
 acoustic_similarity_judgments %<>%
@@ -33,8 +47,23 @@ imitation_matches %<>%
   recode_survey_type() %>%
   add_chance()
 
+n_matching_imitations <- count_subjects(imitation_matches)
+
+data("transcriptions")
+
+n_transcribers <- count_subjects(transcriptions)
+n_imitations_transcribed <- count_imitations(transcriptions)
+n_transcriptions_per_imitation <- transcriptions %>%
+  count(message_id) %>%
+  .$n %>%
+  mean() %>%
+  round(0)
+n_transcriptions <- nrow(transcriptions)
+
 data("transcription_distances")
 data("transcription_matches")
+
+n_transcription_match_subjs <- count_subjects(transcription_matches)
 
 message_id_map <- select(imitations, message_id, chain_name, seed_id, generation)
 
@@ -64,6 +93,8 @@ recode_word_type <- . %>%
  recode_message_type()
 
 data("learning_sound_names")
+n_lsn_subjs <- count_subjects(learning_sound_names)
+
 learning_sound_names %<>%
  mutate(rt = ifelse(is_correct == 1, rt, NA),
         is_error = 1 - is_correct) %>%
@@ -104,12 +135,8 @@ lsn_transition %<>%
 outliers <- c("LSN102", "LSN148", "LSN104", "LSN147")
 learning_sound_names %<>% filter(!(subj_id %in% outliers))
 
-# helper functions -------------------------------------------------------------
-count_imitations <- . %>% .$message_id %>% unique() %>% length()
-count_subjects <- . %>% .$subj_id %>% na.omit() %>% unique() %>% length()
-
 # ggplot theme, colors, and scales ---------------------------------------------
-base_theme <- theme_minimal(base_size=11)
+base_theme <- theme_minimal(base_size=14)
 
 colors <- RColorBrewer::brewer.pal(4, "Set2")
 names(colors) <- c("blue", "orange", "green", "pink")
@@ -145,23 +172,71 @@ scale_color_message_label_2 <- scale_color_manual(
 )
 
 chance_line <- geom_hline(yintercept = 0.25, lty = 2, alpha = 0.4, size = 1)
+chance_label <- annotate("text", x = 1, y = 0.26, label = "chance",
+                         size = 7, vjust = -0.1, fontface = "italic")
 ylim_gts <- c(0.15, 0.75)
-scale_y_gts_accuracy <- scale_y_continuous("Accuracy", breaks = c(0.25, seq(0, 1, by = 0.1)),
+scale_y_gts_accuracy <- scale_y_continuous("Accuracy", breaks = seq(0, 1, by = 0.1),
                                            labels = scales::percent)
 
-# ---- collecting-imitations ---------------------------------------------------
-n_all_imitations <- count_imitations(imitations)
-n_removed <- imitations %>%
-  filter(rejected == "True") %>%
-  count_imitations()
-n_final_imitations <- imitations %>%
-  filter(rejected == "False") %>%
-  count_imitations()
-n_branches <- imitations$first_gen_id %>% unique() %>% na.omit() %>% length()
+# reporting statistical results ------------------------------------------------
+lmer_mod_results <- function(lmertest_mod, param) {
+  results <- broom::tidy(lmertest_mod, effects = "fixed") %>%
+    filter(term == param) %>%
+    as.list()
+  
+  lmertest_summary <- lmerTest::summary(lmertest_mod) %>%
+    .$coefficients %>%
+    as.data.frame()
+  lmertest_summary$term <- rownames(lmertest_summary)
+  lmertest_results <- lmertest_summary %>%
+    filter(term == param) %>%
+    as.list()
+  
+  results$df <- lmertest_results[["df"]]
+  results$p_value <- lmertest_results[["Pr(>|t|)"]]
+  
+  if (results$p_value < 0.001) {
+    results$p_value_str <- "_p_ < 0.001"
+  } else {
+    results$p_value_str <- paste("_p_ = ", round(results$p_value, 3))
+  }
+  
+  sprintf("_b_ = %.2f (%.2f), _t_(%.1f) = %.2f, %s",
+          results$estimate, results$std.error, results$df, results$statistic, results$p_value_str)
+}
 
+
+glmer_mod_results <- function(glmer_mod, param, odds = FALSE) {
+  results <- broom::tidy(glmer_mod, effects = "fixed") %>%
+    filter(term == param) %>%
+    as.list()
+  
+  if (results$p.value < 0.001) {
+    results$p_value_str <- "_p_ < 0.001"
+  } else {
+    results$p_value_str <- paste("_p_ = ", round(results$p.value, 3))
+  }
+  
+  if (odds == TRUE) {
+    results["odds"] <- log(results$estimate)
+    formatted = sprintf("_b_ = %.2f (%.2f) log-odds, odds = %.2f, _z_ = %.2f, %s",
+                        results$estimate, results$std.error, results$odds, results$statistic, results$p_value_str)
+  } else {
+    formatted = sprintf("_b_ = %.2f (%.2f) log-odds, _z_ = %.2f, %s",
+                        results$estimate, results$std.error, results$statistic, results$p_value_str)
+  }
+  formatted
+}
+
+
+# ---- collecting-imitations ---------------------------------------------------
 similarity_judgments_mod <- lmer(
   similarity_z ~ edge_generation_n + (edge_generation_n|name) + (edge_generation_n|category),
   data = acoustic_similarity_judgments
+)
+
+similarity_judgments_lmertest_mod <- lmerTest::lmer(
+  formula(similarity_judgments_mod), data = similarity_judgments_mod@frame
 )
 
 similarity_judgments_preds <- data_frame(edge_generation_n = 1:7) %>%
@@ -177,20 +252,19 @@ similarity_judgments_means <- acoustic_similarity_judgments %>%
 set.seed(949)
 gg_similarity_judgments <- ggplot(similarity_judgments_means) +
   aes(x = edge_generations, y = similarity_z) +
-  geom_point(aes(color = category), position = position_jitter(0.6, 0.0),
-             size = 2, alpha = 0.8) +
+  geom_point(aes(color = category), position = position_jitter(0.2, 0.0),
+             size = 2.5, alpha = 0.8) +
   geom_smooth(aes(group = 1, ymin = similarity_z - se, ymax = similarity_z + se),
               data = similarity_judgments_preds, stat = "identity",
               alpha = 0.2, color = "gray") +
   scale_x_discrete("Generation") +
-  scale_y_continuous("Acoustic similarity") +
+  scale_y_continuous("Acoustic similarity (z-score)") +
   scale_color_brewer("Category", palette = "Set2") +
+  coord_cartesian(ylim = c(-0.6, 0.8)) +
   base_theme +
   theme(legend.position = "top")
 
 # ---- matching-imitations -----------------------------------------------------
-n_matching_imitations <- count_subjects(imitation_matches)
-
 imitation_matches_overall_mod <- glmer(
   is_correct ~ offset(chance_log) + generation_1 + (generation_1|chain_name/seed_id),
   family = "binomial", data = imitation_matches
@@ -226,27 +300,18 @@ gg_match_to_seed <- ggplot(imitation_matches) +
   scale_y_gts_accuracy +
   scale_color_distractors +
   chance_line +
-  annotate("text", x = 10, y = 0.26, label = "chance",
-                         size = 7, vjust = -0.1, fontface = "italic", alpha = 0.4) +
+  chance_label +
   coord_cartesian(xlim = c(-0.2, 7.2), ylim = ylim_gts) +
   base_theme +
   theme(legend.position = c(0.8, 0.85))
 
 # ---- transcriptions
-n_transcribers <- count_subjects(transcriptions)
-n_imitations_transcribed <- count_imitations(transcriptions)
-n_transcriptions_per_imitation <- transcriptions %>%
-  count(message_id) %>%
-  .$n %>%
-  mean() %>%
-  round(0)
-n_transcriptions <- nrow(transcriptions)
-
-n_transcribers <- count_subjects(transcriptions)
-n_transcription_match_subjs <- count_subjects(transcription_matches)
-
 orthographic_distance_mod <- lmer(distance ~ message_c + (message_c|chain_name/seed_id),
                                   data = transcription_distances)
+
+orthographic_distance_lmertest_mod <- lmerTest::lmer(
+  formula(orthographic_distance_mod), data = orthographic_distance_mod@frame
+)
 
 orthographic_distance_preds <- data_frame(message_c = c(-0.5, 0.5)) %>%
   cbind(., predictSE(orthographic_distance_mod, newdata = ., se = TRUE)) %>%
@@ -264,9 +329,10 @@ gg_distance <- ggplot(transcription_distances) +
                 size = 1.4, width = 0.1) +
   scale_x_discrete("Generation",
                    labels = c("First", "Last")) +
-  scale_y_continuous("Distance between transcriptions") +
+  scale_y_continuous("Distance between transcriptions", breaks = seq(0, 1, by = 0.2)) +
   scale_color_manual(values = imitation_gen_colors) +
   scale_fill_manual(values = imitation_gen_colors) +
+  coord_cartesian(ylim = c(0.0, 0.8)) +
   base_theme +
   theme(legend.position = "none")
 
@@ -306,21 +372,26 @@ preds <- cbind(x_preds, y_preds) %>%
   left_join(message_labels)
 
 gg_match_transcriptions <- ggplot(preds) +
-  aes(question_c, is_correct, question_type, fill=question_type) +
-  geom_bar(stat = "identity", width = 0.95, alpha = 0.6) +
-  geom_linerange(aes(ymin = is_correct - se, ymax = is_correct + se)) +
+  aes(question_c, is_correct) +
+  geom_bar(aes(fill = question_type), stat = "identity", width = 0.95, alpha = 0.6) +
+  geom_linerange(aes(group = question_type, ymin = is_correct - se, ymax = is_correct + se)) +
   scale_x_continuous("Question type", breaks = c(-0.5, 0.5), labels = c("True seed", "Category match")) +
   scale_y_gts_accuracy +
   scale_fill_manual("", values = unname(colors[c("blue", "green")])) +
   chance_line +
+  geom_text(aes(label = label),
+            data = data.frame(message_label_2 = "First generation", question_c = -0.7,
+                              is_correct = 0.26, label = "chance"),
+            fontface = "italic") +
   coord_cartesian(ylim = ylim_gts) +
   facet_wrap("message_label_2") +
   base_theme +
-  theme(legend.position = "none", axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(legend.position = "none",
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank())
+gg_match_transcriptions
 
 # ---- category-learning
-n_lsn_subjs <- count_subjects(learning_sound_names)
-
 first_last_gen <- filter(learning_sound_names, message_type != "sound_effect") %>%
   mutate(block_ix_sqr = block_ix^2)
 
@@ -328,6 +399,10 @@ after_first_block <- filter(first_last_gen, block_ix > 1)
 lsn_after_first_block_mod <- lmer(
   rt ~ message_c + block_ix + (1 + block_ix|subj_id),
   data = after_first_block)
+
+lsn_after_first_block_lmertest_mod <- lmerTest::lmer(
+  formula(lsn_after_first_block_mod), data = lsn_after_first_block_mod@frame
+)
 
 lsn_quad_mod <- lmer(
   rt ~ message_c * (block_ix + block_ix_sqr) + (block_ix + block_ix_sqr|subj_id),
@@ -349,5 +424,40 @@ rt_plot <- ggplot(first_last_gen) +
   scale_x_block_ix +
   scale_y_rt +
   scale_color_message_label_2 +
+  coord_cartesian(ylim = c(600, 1200)) +
   base_theme +
   theme(legend.position = c(0.8, 0.7))
+
+transition_mod <- lmer(
+  rt ~ block_transition_c * message_c + block_ix + (block_ix|subj_id),
+  data = lsn_transition
+)
+
+transition_lmertest_mod <- lmerTest::lmer(
+  formula(transition_mod), data = transition_mod@frame
+)
+
+transition_preds <- expand.grid(block_transition_c = c(-0.5, 0.5),
+                                message_c = c(-0.5, 0.5),
+                                block_ix = 3) %>%
+  cbind(., predictSE(transition_mod, ., se = TRUE)) %>%
+  rename(rt = fit, se = se.fit) %>%
+  recode_block_transition() %>%
+  recode_message_type()
+
+dodger <- position_dodge(width = 0.1)
+
+gg_transition <- ggplot(lsn_transition) +
+  aes(block_transition_label, rt, color = message_type) +
+  geom_linerange(aes(ymin = rt - se, ymax = rt + se),
+                 data = transition_preds,
+                 position = dodger, show.legend = FALSE,
+                 size = 2) +
+  geom_line(aes(group = message_type), data = transition_preds,
+            position = dodger, size = 2) +
+  scale_x_discrete("Block transition", labels = c("Before", "After")) +
+  scale_y_rt +
+  scale_color_message_label_2 +
+  coord_cartesian(ylim = c(600, 1200)) +
+  base_theme +
+  theme(legend.position = c(0.85, 0.8))
